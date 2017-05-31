9bb72454814061eb05b9bf62e358c713c0c7b5ac
Leonardo Kameya <leonardo.kameya@gmail.com>
2017-05-31 10:38:10 -0400

    Creating reusable form components

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,26 +1,30 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { employeeUpdate, employeeCreate } from '../actions';
+import { employeeUpdate, employeeSave } from '../actions';
 import { Card, CardSection, Button } from './common';
 import EmployeeForm from './EmployeeForm';
 
-
-
-class EmployeeCreate extends Component {
+class EmployeeEdit extends Component {
+    componentWillMount() {
+        _.each(this.props.employee, (value, prop) => {
+            this.props.employeeUpdate({ prop, value });
+        });
+    }
 
     onButtonPress() {
         const { name, phone, shift } = this.props;
 
-        this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+        this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
     }
 
     render() {
         return (
             <Card>
-                <EmployeeForm {...this.props} />
+                <EmployeeForm />
                 <CardSection>
                     <Button onPress={this.onButtonPress.bind(this)}>
-                        Create
+                        Save Changes
                     </Button>
                 </CardSection>
             </Card>
@@ -28,10 +32,10 @@ class EmployeeCreate extends Component {
     }
 }
 
-
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
     const { name, phone, shift } = state.employeeForm;
+
     return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate, employeeCreate })(EmployeeCreate);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
